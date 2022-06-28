25a658c25d02f881fba3d1b4bb1cf03b1bdd0940
Ethan Meyers <ewm1230@gmail.com>
2022-06-28 16:46:09 -0500

    Removed index column and incorrect calculated times

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # @Author: Ethan Meyers
-# Email: ewm1230@gmail.com
-# Phone#: 847-212-2264
+# @Email: ewm1230@gmail.com
+# @Phone: 847-212-2264
 # @Date: 06/13/2022
 
 # This script takes a path to an Excel file
@@ -58,7 +58,7 @@ def resource_path(relative_path):
 
 def deleteRows(FilePath):
     # Gets data from Excel sheet and removes elements that are in the 1099 drivers list
-    df = pd.read_excel(FilePath, sheet_name='Duty Time', header=8)
+    df = pd.read_excel(FilePath, sheet_name='Duty Time', header=8, index_col=False)
     # df2 = pd.read_excel(FilePath, sheet_name='Sheet1', header=0)
     df2 = getW2()
     df = df[df['Login'].str.lower().isin(df2['W2 Drivers'])]
@@ -76,7 +76,7 @@ def deleteRows(FilePath):
 
     # Print DF
     writer = pd.ExcelWriter(FilePath, engine='openpyxl')
-    df.to_excel(writer, sheet_name='Output')
+    df.to_excel(writer, sheet_name='Output', index=False)
     writer.save()
 
 def formatCols(FilePath):
@@ -86,30 +86,30 @@ def formatCols(FilePath):
     # Using openpyxl to format the new Excel sheet
 
     # Format Source Code: https://openpyxl.readthedocs.io/en/stable/_modules/openpyxl/styles/numbers.html
+    for cell in worksheet['C']:
+        cell.number_format = numbers.FORMAT_DATE_XLSX14
+    for cell in worksheet['F']:
+        cell.number_format = numbers.FORMAT_DATE_XLSX14
+
     for cell in worksheet['D']:
-        cell.number_format = numbers.FORMAT_DATE_XLSX14
+        cell.number_format = numbers.FORMAT_DATE_TIME2
     for cell in worksheet['G']:
-        cell.number_format = numbers.FORMAT_DATE_XLSX14
-
-    for cell in worksheet['E']:
-        cell.number_format = numbers.FORMAT_DATE_TIME2
-    for cell in worksheet['H']:
         cell.number_format = numbers.FORMAT_DATE_TIME2
 
+    worksheet.column_dimensions['A'].width = 20
     worksheet.column_dimensions['B'].width = 20
     worksheet.column_dimensions['C'].width = 20
     worksheet.column_dimensions['D'].width = 20
     worksheet.column_dimensions['E'].width = 20
     worksheet.column_dimensions['F'].width = 20
     worksheet.column_dimensions['G'].width = 20
-    worksheet.column_dimensions['H'].width = 20
 
     # Alternates filling each row based off of a new username
     yellowFill = PatternFill(start_color='00FFFF00', end_color='00FFFF00', fill_type='solid')
-    currentName = worksheet['B2'].value
-    colIndex = 2
+    currentName = worksheet['A2'].value
+    colIndex = 1
     fill = False
-    for i in range(2, worksheet.max_row+1):
+    for i in range(colIndex, worksheet.max_row+1):
         if worksheet.cell(row=i, column=colIndex).value == currentName and fill:
             for j in range(colIndex, worksheet.max_column+1):
                 worksheet.cell(row=i, column=j).fill = yellowFill
@@ -121,29 +121,29 @@ def formatCols(FilePath):
                     for j in range(colIndex, worksheet.max_column+1):
                         worksheet.cell(row=i, column=j).fill = yellowFill
 
-    # Adds time total column
-    redFill = PatternFill(start_color='00FC0303', end_color='00FC0303', fill_type='solid')
-    orangeFill = PatternFill(start_color='00FAAC02', end_color='00FAAC02', fill_type='solid')
-    currentName = worksheet['B2'].value
-    startIndex = 2
-    fill = False
-    for i in range(2, worksheet.max_row + 2):
-        if worksheet.cell(row=i, column=colIndex).value != currentName:
-            currentName = worksheet.cell(row=i, column=colIndex).value
-            total = timedelta(hours=0)
-            for j in range(startIndex, i):
-                s1 = str(worksheet.cell(row=j, column=5).value)
-                startTime = datetime.strptime(s1, '%Y-%m-%d %H:%M:%S')
-                s2 = str(worksheet.cell(row=j, column=8).value)
-                endTime = datetime.strptime(s2, '%Y-%m-%d %H:%M:%S')
-                total = total + (endTime-startTime)
-            worksheet.cell(row=i - 1, column=9).value = total
-            if fill:
-                worksheet.cell(row=i - 1, column=9).fill = orangeFill
-            else:
-                worksheet.cell(row=i - 1, column=9).fill = redFill
-            fill = not fill
-            startIndex = i
+    # Adds time total column #TODO: Incorrect Times Calculated!!!
+    # redFill = PatternFill(start_color='00FC0303', end_color='00FC0303', fill_type='solid')
+    # orangeFill = PatternFill(start_color='00FAAC02', end_color='00FAAC02', fill_type='solid')
+    # currentName = worksheet['B2'].value
+    # startIndex = 2
+    # fill = False
+    # for i in range(2, worksheet.max_row + 2):
+    #     if worksheet.cell(row=i, column=colIndex).value != currentName:
+    #         currentName = worksheet.cell(row=i, column=colIndex).value
+    #         total = timedelta(hours=0)
+    #         for j in range(startIndex, i):
+    #             s1 = str(worksheet.cell(row=j, column=5).value)
+    #             startTime = datetime.strptime(s1, '%Y-%m-%d %H:%M:%S')
+    #             s2 = str(worksheet.cell(row=j, column=8).value)
+    #             endTime = datetime.strptime(s2, '%Y-%m-%d %H:%M:%S')
+    #             total = total + (endTime-startTime)
+    #         worksheet.cell(row=i - 1, column=9).value = total
+    #         if fill:
+    #             worksheet.cell(row=i - 1, column=9).fill = orangeFill
+    #         else:
+    #             worksheet.cell(row=i - 1, column=9).fill = redFill
+    #         fill = not fill
+    #         startIndex = i
 
     workbook.save(FilePath)
 
